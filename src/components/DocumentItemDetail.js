f5e30fd1ecf1dfa1d59dc8772906d79d4b6b06f7
Adam Michalunio <amichalunio@gmail.com>
2017-04-07 15:49:48 +0600

    Layout

--- src/components/DocumentItemDetail.js
+++ src/components/DocumentItemDetail.js
@@ -23,27 +23,32 @@ class DocumentItemDetail extends Component {
   }
 
   renderArticles() {
-    return this.props.documentDetail.data.articles.map((art) => (
-    <li key={art.id.toString()}>{art.artname1} {art.artprice} {art.arttaxlabel} {art.artunit} {art.quantity} {art.nettovalue} {art.bruttovalue}</li> ))
+    var id = 1
+    return this.props.documentDetail.data.articles.map((art) => (      
+      <tr key={art.id.toString()}>
+        <td>{id++}</td>      
+        <td>{art.artname1}</td>
+        <td>{art.artprice}</td>
+        <td>{art.arttaxlabel}</td>
+        <td>{art.artunit}</td>
+        <td>{art.quantity}</td>
+        <td>{art.nettovalue}</td>
+        <td>{art.bruttovalue}</td>
+      </tr>
+    ))
   }
-//w3-panel w3-border w3-light-grey w3-round-large
-//w3-panel w3-light-grey w3-leftbar w3-border-blue
   render() {
     const w3class = "w3-panel w3-light-grey w3-leftbar w3-border-blue"
     return <div className={w3class + " div-detail " + this.state.classNameAnim}>
         <span onClick={this.onClose} className="w3-button w3-display-topright">X</span>
-        <div><ul>{this.props.documentDetail.data ? this.renderArticles() : ''}</ul></div>
-      </div>
-  }
 
-  /*render() {
-    return <div className={"div-detail " + this.state.classNameAnim}>DETAIL
-        <input type="button" value="Close" onClick={this.onClose}/>
-        <div className="w3-sand"><ul>{this.props.documentDetail.data ? this.renderArticles() : ''}</ul></div>
+        <table className="w3-table-all w3-card-2">
+        <tr><th></th><th></th><th></th><th></th><th></th><th></th><th></th><th></th></tr>
+        {this.props.documentDetail.data ? this.renderArticles() : ''}
+        </table>
+
       </div>
-  }*/
-  
+  }  
 }
-//w3-panel w3-sand w3-display-container
 
 export default DocumentItemDetail
