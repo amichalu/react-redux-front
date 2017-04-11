59b7aa44fd42c13df1c1cb84da97de228174ffc5
Adam Michalunio <amichalunio@gmail.com>
2017-04-11 14:41:12 +0600

    layout

--- src/components/DocumentItemDetail.js
+++ src/components/DocumentItemDetail.js
@@ -39,15 +39,57 @@ class DocumentItemDetail extends Component {
   }
   render() {
     console.log("DocumentItemDetail.render()")
-    const w3class = "w3-panel w3-leftbar w3-border-blue"
+
+    const d = this.props.document
+    const w3class = "w3-card w3-leftbar w3-border-grey"
     return <div className={w3class + " div-detail " + this.state.classNameAnim}>
         <span onClick={this.onClose} className="w3-button w3-display-topright">X</span>
 
-        <table className="w3-table w3-bordered w3-border"><tbody>
-          <tr className=""><th>Nr</th><th>Article name</th><th>Price</th><th></th><th></th><th></th><th></th><th></th></tr>
-          {this.props.articles.items ? this.renderArticles() : ''}
-        </tbody></table>
-        
+        <header className="w3-container w3-amber">
+          <h5>Invoice Nmb: {d.id}</h5>
+        </header>
+
+        <div className="w3-container w3-padding-small">
+
+          <div className="w3-cell-row">
+
+            <div className="w3-container w3-cell w3-mobile w3-padding-small" style={{width: "50%"}}>
+              <form className="w3-container">
+                <label className="w3-text-gray">Document Nmb</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.id}/>
+                <label className="w3-text-gray">Place of Invoice</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.location}/>
+                <label className="w3-text-gray">Date</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.date}/>
+                <label className="w3-text-gray">Payment</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.paymethod_name}/>
+                <label className="w3-text-gray">Period from:</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.period_startdate}/>
+                <label className="w3-text-gray">Period to:</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.period_enddate}/>                
+              </form>
+            </div>
+
+            <div className="w3-container w3-cell w3-mobile" style={{width: "50%"}}>
+              <form className="w3-container">
+                <label className="w3-text-gray">Customer name I</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.custname1}/>
+                <label className="w3-text-gray">Customer Name II</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.custname2}/>
+                <label className="w3-text-gray">VAT ID</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.custnip}/>
+                <label className="w3-text-gray">Internal Account Nmb</label>
+                <input className="w3-input w3-padding-small" type="text" value={d.custaccnmb}/>
+              </form>
+            </div>
+          
+          </div>
+
+          <table className="w3-table w3-bordered w3-border" style={{ margin: "5px 0px"}}><tbody>
+            <tr className=""><th>Nr</th><th>Article name</th><th>Price</th><th></th><th></th><th></th><th></th><th></th></tr>
+            {this.props.articles.items ? this.renderArticles() : ''}
+          </tbody></table>
+        </div>
 
       </div>
   }  
